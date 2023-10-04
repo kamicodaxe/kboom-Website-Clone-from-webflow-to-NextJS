e85bc708bfb67caa848f3bd58dd9fe8b4c15d2ee
Loïc Kami <lkami@bogial.com>
2023-10-04 16:10:32 +0200

    Add ViewSettings component for dark/light mode management

---
+++ components/ui/view-settings.tsx
@@ -0,0 +1,61 @@
+"use client"
+/**
+ * ViewSettings component
+ */
+import * as React from "react"
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select"
+import { SelectLanguage } from "./selectLanguage";
+
+
+export interface ViewSettingsProps {
+}
+
+const ViewSettings: React.FC<ViewSettingsProps> = ({ }) => {
+	// Create a state for the dark mode
+	const [darkMode, setDarkMode] = React.useState(true);
+
+	// if (typeof window !== 'undefined') {
+	// 	const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+	// 	setDarkMode(prefersDarkMode.matches);
+	// }
+
+	// Use an effect to add or remove the 'dark' class from the root element
+	React.useEffect(() => {
+		const root = document.documentElement;
+		if (darkMode) {
+			root.classList.add('dark');
+		} else {
+			root.classList.remove('dark');
+		}
+	}, [darkMode]);
+
+
+	return (
+		<section className=" text-black dark:text-white flex justify-center">
+			<Select defaultValue={darkMode ? "dark" : "light"} onValueChange={v => {
+				setDarkMode(v == "dark" ? true : false)
+			}}>
+				<SelectTrigger className="w-[180px]">
+					Mode
+					<SelectValue placeholder="English" />
+				</SelectTrigger>
+				<SelectContent>
+					<SelectItem value="dark">Dark</SelectItem>
+					<SelectItem value="light">Light</SelectItem>
+				</SelectContent>
+			</Select>
+		</section>
+	);
+};
+
+
+
+ViewSettings.displayName = "ViewSettings"
+
+export { ViewSettings }
