31fa6461ddc4ba811cec6d6c2c777acac755e305
Loïc Kami <lkami@bogial.com>
2023-10-04 13:56:32 +0200

    Update BlogPost and BlogPost Loading - Add dynamic Categories - Add dynamic tags

--- app/[lang]/blog/[slug]/loading.tsx
+++ app/[lang]/blog/[slug]/loading.tsx
@@ -1,19 +1,11 @@
+import { Navbar } from "@/components/ui/navbar";
 import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
 	return (
-		<main className="bg-gray-800 text-gray-50">
+		<main className="dark:bg-gray-800 text-gray-50">
 
-			<nav className="flex justify-between py-5 px-4 mx-auto">
-				<Skeleton className="w-32 h-8" />
-				<Skeleton className="h-10 w-32" />
-				<Skeleton className="h-10 w-32" />
-				<button className="p-4 lg:hidden">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-8 h-8 text-white dark:text-white">
-						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-					</svg>
-				</button>
-			</nav>
+			<Navbar.Skeleton />
 
 			<div className="flex justify-center items-center">
 				<div className="container mx-auto p-8">
--- app/[lang]/blog/[slug]/page.tsx
+++ app/[lang]/blog/[slug]/page.tsx
@@ -3,25 +3,38 @@ import { Navbar } from '@/components/ui/navbar'
 import { getDictionary } from '../../dictionary'
 import Breadcrumb from '@/components/ui/breadcrumb'
 import { Newsletter } from '@/components/ui/newsletter'
+import { Post } from '@/lib/types'
+import clsx from 'clsx'
+import BlogPosts from '@/components/blog/blog-posts'
 
 interface BlogPostProps {
   params: {
     lang: string
+    slug: string
   }
 }
 
+const getPost: (slug: string) => Post = (slug) => {
+  const post = posts.find(p => p.slug == slug)
+  return post || posts[0]
+}
+
+const getColorClass: (index: number) => string = (index) => {
+  let classes = ["bg-yellow-500", "bg-indigo-500", "bg-green-500", "bg-amber-500"]
+  return classes[index % classes.length]
+}
 
 export default async function BlogPost(props: BlogPostProps) {
   const translations = await getDictionary(props.params.lang)
   console.log(JSON.stringify(props, null, 2))
-  const post = posts[0]
+  const post = getPost(props.params.slug)
 
   return (
     <main className="bg-white dark:bg-[#262c3a]">
       <Navbar translations={translations} />
 
       <section className="bg-[#fed351] text-black dark:text-white">
-        <div className="container mx-auto flex flex-col items-center pt-16 text-center max-w-5xl">
+        <div className="container mx-auto flex flex-col items-center py-16 text-center max-w-5xl">
           <h1 className="text-5xl font-[800] md:leading-[82px] md:text-7xl">
             {post.title}
           </h1>
@@ -38,27 +51,42 @@ export default async function BlogPost(props: BlogPostProps) {
         </div>
       </section>
 
-      <div className="container mx-auto flex flex-col py-16 text-gray-800 dark:text-gray-200 max-w-3xl">
+      <div className="container mx-auto flex flex-col py-16 items-stretch text-gray-800 dark:text-gray-200 max-w-3xl">
         <img src="https://via.placeholder.com/800x400" alt="Blog Post" className="w-full h-64 object-cover mb-6" />
         <p className="mb-6 self-start">Published on August 25, 2023 by {post.author.name} </p>
+
         <div dangerouslySetInnerHTML={{
           __html: post.content
-        }} className="prose" />
+        }} className="prose w-full" />
 
         <div className="border-t border-gray-300 pt-4">
           <h2 className="text-xl font-bold mb-2">Categories</h2>
           <div className="flex flex-wrap gap-2 mb-4">
-            <span className="bg-blue-500 text-white px-2 py-1 rounded">Esports</span>
+            {
+              post.categories.map((cat, index) => (
+                <span key={cat.id} className={clsx(
+                  "text-white px-2 py-1 rounded",
+                  getColorClass(index)
+                )}>{cat.name}</span>
+              ))
+            }
           </div>
           <h2 className="text-xl font-bold mb-2">Tags</h2>
           <div className="flex flex-wrap gap-2">
-            <span className="bg-yellow-500 text-white px-2 py-1 rounded">Gaming</span>
-            <span className="bg-indigo-500 text-white px-2 py-1 rounded">Strategy</span>
-            <span className="bg-green-500 text-white px-2 py-1 rounded">Tactics</span>
+            {
+              post.tags.map((tag, index) => (
+                <span key={tag.id} className={clsx(
+                  "text-white px-2 py-1 rounded",
+                  getColorClass(index)
+                )}>{tag.name}</span>
+              ))
+            }
           </div>
         </div>
 
       </div>
+
+      <BlogPosts title='Read More' subtitle='' posts={posts} showCount={4} />
 
       <Newsletter translations={translations} />
 
