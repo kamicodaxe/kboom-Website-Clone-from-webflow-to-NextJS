638eda52ba0242b4ce1dc2877dfd95388ce6729c
Loïc Kami <lkami@bogial.com>
2023-10-04 03:36:11 +0200

    Add database.ts with test data

---
+++ app/api/database.ts
@@ -0,0 +1,262 @@
+import 'server-only'
+
+const posts = [
+	{
+		"id": 123,
+		"title": "Game Analysis: Strategies and Tactics of Popular Esports Titles",
+		"slug": "game-analysis-strategies-and-tactics-of-popular-esports-titles",
+		"content": "<div class='content'><h2>1. League of Legends: The Art of Team Coordination</h2><p><strong>Introduction:</strong> League of Legends (LoL), developed by Riot Games, is a multiplayer online battle arena (MOBA) game that demands exceptional teamwork and communication. With different roles like tanks, mages, and marksmen, LoL requires teams to craft well-rounded compositions, ensuring balance between offense, defense, and support.</p><p><strong>Strategies:</strong></p><ul><li><strong>Champion Synergy:</strong> Understanding which champions synergize best with one another is crucial. Teams often strategize around powerful combinations to secure advantages in team fights.</li><li><strong>Map Control:</strong> Controlling objectives like Dragon and Baron Nashor provides global buffs. Teams must strategically time and contest these objectives to gain an edge.</li></ul><p><strong>Tactics:</strong></p><ul><li><strong>Ganking:</strong> Surprise attacks, known as ganks, involve coordinated strikes by multiple teammates to overwhelm opponents. Timing and execution are vital.</li><li><strong>Split Pushing:</strong> Dividing the team to push multiple lanes simultaneously can force opponents into difficult decisions, creating opportunities for strategic map control.</li></ul><h2>2. Counter-Strike: Global Offensive (CS:GO): Precision and Timing</h2><p><strong>Introduction:</strong> CS:GO, a first-person shooter developed by Valve Corporation, revolves around counter-terrorists and terrorists battling in intense, objective-based scenarios. This game emphasizes precise aiming, tactical maneuvers, and clever use of utilities.</p><p><strong>Strategies:</strong></p><ul><li><strong>Economy Management:</strong> Teams need to carefully manage their in-game currency to purchase weapons and utilities. Strategic decisions on when to buy, save, or force-buy can influence entire matches.</li><li><strong>Map Control:</strong> Understanding map layouts and establishing control over critical points is essential. Defensive setups and attacking routes are meticulously planned.</li></ul><p><strong>Tactics:</strong></p><ul><li><strong>Smoke and Flashbang Usage:</strong> Using smoke grenades and flashbangs strategically can disrupt enemy visibility and create openings for offensive plays or defensive retreats.</li><li><strong>Boosts and Map Exploits:</strong> Creative boosting onto ledges or finding unexpected map exploits can surprise opponents, providing unexpected advantages.</li></ul><h2>3. Dota 2: Complex Strategies and Hero Diversity</h2><p><strong>Introduction:</strong> Dota 2, another popular MOBA developed by Valve Corporation, emphasizes deep hero customization and strategic diversity. Each match is a battle between two teams, each seeking to destroy the opposing team's ancient structure.</p><p><strong>Strategies:</strong></p><ul><li><strong>Hero Drafting:</strong> The drafting phase involves picking and banning heroes. Teams strategize by countering opponents' picks while ensuring their lineup has a coherent strategy.</li><li><strong>Lane Control:</strong> Dota 2 features multiple lanes and a jungle area. Teams must control these lanes, secure farm for their carries, and limit enemy resources.</li></ul><p><strong>Tactics:</strong></p><ul><li><strong>Rotations:</strong> Skilled players frequently rotate between lanes to support teammates, gank enemies, or secure objectives. Timely rotations can turn the tide of battles.</li><li><strong>Warding and Map Awareness:</strong> Placing wards to gain vision and awareness of enemy movements is crucial. Teams with superior map control can anticipate enemy actions and plan accordingly.</li></ul><h2>4. Overwatch: Team Synergy and Hero Switching</h2><p><strong>Introduction:</strong> Overwatch, a team-based first-person shooter developed by Blizzard Entertainment, features a diverse cast of heroes, each with unique abilities. Team synergy, hero switching, and precise coordination are paramount.</p><p><strong>Strategies:</strong></p><ul><li><strong>Team Composition:</strong> Teams craft compositions based on tanks, damage dealers, and supports. Balanced compositions with synergistic abilities are pivotal for success.</li><li><strong>Ultimate Economy:</strong> Ultimates, powerful abilities charged over time, must be managed strategically. Teams coordinate their ultimates to execute devastating combo plays.</li></ul><p><strong>Tactics:</strong></p><ul><li><strong>Hero Swapping:</strong> Overwatch allows players to switch heroes mid-match to adapt to the enemy's composition. Tactical hero swaps counter opponents and bolster team strengths.</li><li><strong>Flanking and Ambushes:</strong> Heroes with flanking abilities can surprise enemies from unexpected angles. Coordinated ambushes catch opponents off-guard, creating advantageous engagements.</li></ul><h2>Conclusion: Unlocking the Secrets of Victory</h2><p>In the world of esports, mastering these strategies and tactics is akin to unraveling the mysteries of an intricate puzzle. Each game demands a unique approach, and successful teams blend mechanical skill, strategic acumen, and seamless teamwork. As you dive into the realm of competitive gaming, understanding these elements will not only enrich your viewing experience but also inspire you to appreciate the strategic brilliance behind every esports triumph. So, gear up, strategize wisely, and get ready to witness the exhilarating world of esports unfold before your eyes!</p></div>",
+		"date": "2023-10-01T12:00:00Z",
+		"author": {
+			"id": 1,
+			"name": "Loïc Kami"
+		},
+		"categories": [
+			{
+				"id": 5,
+				"name": "Esports"
+			}
+		],
+		"tags": [
+			{
+				"id": 10,
+				"name": "Gaming"
+			},
+			{
+				"id": 11,
+				"name": "Strategy"
+			},
+			{
+				"id": 12,
+				"name": "Tactics"
+			}
+		]
+	},
+
+	{
+		"id": 124,
+		"title": "The Rise of Esports: A Global Phenomenon",
+		"slug": "the-rise-of-esports-a-global-phenomenon",
+		"content": "<div class='content'><p>Esports, once a niche interest, has transformed into a global phenomenon shaping the landscape of competitive gaming. This article explores the origins of esports, its rapid growth, and the impact it has on the gaming industry and pop culture.</p><p>From local tournaments to international championships, esports has captured the hearts of millions of fans worldwide. We delve into the rise of esports organizations, the dedication of professional players, and the immense fan base that follows their favorite teams.</p><p>Join us on a journey through the evolution of esports, highlighting memorable moments, legendary players, and the future trends that will continue to propel esports into new heights.</p></div>",
+		"date": "2023-10-05T14:30:00Z",
+		"author": {
+			"id": 2,
+			"name": "Alice Johnson"
+		},
+		"categories": [
+			{
+				"id": 5,
+				"name": "Esports"
+			}
+		],
+		"tags": [
+			{
+				"id": 10,
+				"name": "Gaming"
+			},
+			{
+				"id": 13,
+				"name": "Esports History"
+			}
+		]
+	},
+	{
+		"id": 127,
+		"title": "Esports Sponsorships: A Win-Win Partnership",
+		"slug": "esports-sponsorships-a-win-win-partnership",
+		"content": "<div class='content'><p>Esports sponsorships have become a driving force behind the industry's growth, benefiting both players and sponsors. In this article, we explore the symbiotic relationship between esports organizations and sponsors, examining the advantages for both parties.</p><p>From brand visibility to financial support, esports sponsorships offer players and teams the resources they need to compete at the highest level. Sponsors, in turn, gain exposure to a vast audience of dedicated fans, leading to increased brand recognition and loyalty.</p><p>Join us as we analyze successful esports sponsorship campaigns, highlighting innovative strategies and the positive impact they have on the gaming community. Discover how these partnerships contribute to the thriving ecosystem of competitive gaming.</p></div>",
+		"date": "2023-10-20T09:30:00Z",
+		"author": {
+			"id": 5,
+			"name": "Sophia Lee"
+		},
+		"categories": [
+			{
+				"id": 5,
+				"name": "Esports"
+			},
+			{
+				"id": 8,
+				"name": "Marketing"
+			}
+		],
+		"tags": [
+			{
+				"id": 10,
+				"name": "Gaming"
+			},
+			{
+				"id": 16,
+				"name": "Sponsorships"
+			}
+		]
+	},
+	{
+		"id": 128,
+		"title": "The Future of Esports: Technological Innovations and Beyond",
+		"slug": "the-future-of-esports-technological-innovations-and-beyond",
+		"content": "<div class='content'><p>As technology continues to advance, the future of esports holds limitless possibilities. This article explores the technological innovations reshaping the esports landscape, from virtual reality experiences to AI-powered analytics.</p><p>We delve into the impact of 5G connectivity on online gaming, enabling seamless gameplay and enhanced live streaming. Augmented reality interfaces and immersive VR tournaments are redefining the fan experience, bringing spectators closer to the action than ever before.</p><p>Join us as we envision the future of esports, discussing the integration of blockchain technology, esports in the metaverse, and the potential of esports education. Embark on a journey into the cutting-edge advancements that promise an exciting and transformative era for competitive gaming.</p></div>",
+		"date": "2023-10-25T13:45:00Z",
+		"author": {
+			"id": 6,
+			"name": "Michael Chen"
+		},
+		"categories": [
+			{
+				"id": 5,
+				"name": "Esports"
+			},
+			{
+				"id": 9,
+				"name": "Technology"
+			}
+		],
+		"tags": [
+			{
+				"id": 10,
+				"name": "Gaming"
+			},
+			{
+				"id": 17,
+				"name": "Future Trends"
+			}
+		]
+	},
+	{
+		"id": 126,
+		"title": "The Psychology of Esports: Mental Strategies for Success",
+		"slug": "the-psychology-of-esports-mental-strategies-for-success",
+		"content": "<div class='content'><p>Esports isn't just about quick reflexes and accurate aim; it's a mental game that requires strategic thinking, focus, and resilience. This article delves into the psychology behind esports, exploring the mental strategies employed by professional players to achieve success.</p><p>We examine topics such as performance anxiety, stress management, and the importance of teamwork. From pre-game rituals to in-game decision-making, understanding the psychological aspects of esports can provide valuable insights for aspiring players and enthusiasts alike.</p><p>Join us as we unravel the complexities of the human mind in the world of competitive gaming, shedding light on the psychological skills that set champions apart.</p></div>",
+		"date": "2023-10-15T10:15:00Z",
+		"author": {
+			"id": 4,
+			"name": "Emma Turner"
+		},
+		"categories": [
+			{
+				"id": 5,
+				"name": "Esports"
+			},
+			{
+				"id": 7,
+				"name": "Psychology"
+			}
+		],
+		"tags": [
+			{
+				"id": 10,
+				"name": "Gaming"
+			},
+			{
+				"id": 15,
+				"name": "Mental Health"
+			}
+		]
+	},
+	{
+		"id": 125,
+		"title": "The Impact of Esports on Youth Culture",
+		"slug": "the-impact-of-esports-on-youth-culture",
+		"content": "<div class='content'><p>Esports isn't just a form of entertainment; it's a cultural phenomenon that significantly influences youth culture. In this article, we explore the ways esports has become an integral part of modern youth, shaping their lifestyles, interests, and social interactions.</p><p>From esports-inspired fashion trends to the rise of gaming influencers on social media platforms, the impact of esports on youth is undeniable. We examine the positive aspects, such as promoting teamwork and strategic thinking, as well as the challenges, including screen time management and online toxicity.</p><p>Join us as we analyze the multifaceted relationship between esports and youth culture, shedding light on the evolving dynamics of this exciting intersection.</p></div>",
+		"date": "2023-10-10T11:45:00Z",
+		"author": {
+			"id": 3,
+			"name": "David Miller"
+		},
+		"categories": [
+			{
+				"id": 5,
+				"name": "Esports"
+			},
+			{
+				"id": 6,
+				"name": "Youth Culture"
+			}
+		],
+		"tags": [
+			{
+				"id": 10,
+				"name": "Gaming"
+			},
+			{
+				"id": 14,
+				"name": "Youth Trends"
+			}
+		]
+	}
+]
+
+const categories = [
+	{
+		"id": 5,
+		"name": "Esports"
+	},
+	{
+		"id": 6,
+		"name": "Youth Culture"
+	},
+	{
+		"id": 7,
+		"name": "Psychology"
+	},
+	{
+		"id": 9,
+		"name": "Technology"
+	},
+	{
+		"id": 8,
+		"name": "Marketing"
+	}
+]
+
+
+const tags = [
+	{
+		"id": 1,
+		"name": "Technology"
+	},
+	{
+		"id": 2,
+		"name": "Web Development"
+	},
+	{
+		"id": 3,
+		"name": "Programming"
+	},
+	{
+		"id": 4,
+		"name": "JavaScript"
+	},
+	{
+		"id": 5,
+		"name": "Esports"
+	},
+	{
+		"id": 6,
+		"name": "Gaming"
+	},
+	{
+		"id": 7,
+		"name": "Esports Teams"
+	},
+	{
+		"id": 8,
+		"name": "Esports Strategy"
+	},
+	{
+		"id": 9,
+		"name": "Game Analysis"
+	},
+	{
+		"id": 10,
+		"name": "Esports History"
+	}
+]
+
+
+
+export {
+	posts,
+	tags,
+	categories
+};
